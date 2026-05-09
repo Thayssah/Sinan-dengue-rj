c92e59787ac41579aaef9a27dc60a01a32b92b80
thayssa <alvesthayssa79@gmail.com>
2026-05-08 21:27:46 -0300

    codigo principal

--- workspace dengue/maincode.r
+++ workspace dengue/maincode.r
@@ -1,28 +1,15 @@
-
 library(data.table)
 library(readr)
 library(dplyr)
-#library(vroom)
-
-pasta_csv <- "C:/Users/alves/Desktop/csv quebrados" # Onde estão seus 11 CSVs
-arquivo_saida <- "C:/Users/alves/Desktop/csv quebrados/todos_concatenados.csv"
-
-dados_concatenados <- rbindlist(lapply(arquivos_saida, fread), fill = TRUE)
-
-fwrite(dados_concatenados, arquivo_saida, sep = ";", dec = ",")
-#cat(paste0("\n✓ Arquivo salvo em: ", arquivo_saida, "\n"))
-
-
+#-----------------------------------------------------------------------------------------------------------------------------------------
+# FILTRANDO COLUNAS PARA LINHAS ESPECIFICAS 
 arquivos_filtrados <- fread(
   "C:\\Users\\alves\\OneDrive - edu.unirio.br\\Dados sinan dengue\\dados dengue\\workspace dengue\\dados csv\\filtrados.csv",
   sep = ";",
-  select = NULL,  # todas as colunas
+  select = NULL,  
   nrows = Inf,
   verbose = FALSE
-)[ID_AGRAVO == "A90" & SG_UF_NOT == 33]
+)[ID_AGRAVO == "A90" & SG_UF_NOT == 33] #COLUNA IDAGRAVO E COLUNA SG_UF_NOT PARA FILTRAR APENAS PARA A90 E 33 (RIO DE JANEIRO), RESPECTIVAMENTE.
 
-saveRDS(arquivos_filtrados, "C:\\Users\\alves\\OneDrive - edu.unirio.br\\Dados sinan dengue\\dados dengue\\workspace dengue\\dados csv\\filtrados.rds")
-
-
-
-
+saveRDS(arquivos_filtrados, 
+  "C:\\Users\\alves\\OneDrive - edu.unirio.br\\Dados sinan dengue\\dados dengue\\workspace dengue\\dados csv\\filtrados.rds")
