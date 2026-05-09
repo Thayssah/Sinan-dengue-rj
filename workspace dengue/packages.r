dcc3b57e0886df5446d65f52c8830bf6d6858989
thayssa <alvesthayssa79@gmail.com>
2026-05-08 21:28:06 -0300

    pacotes pra baixar

--- workspace dengue/packages.r
+++ workspace dengue/packages.r
@@ -1,3 +1,4 @@
+#-----------------------------------------------------------------------------------------------------------------------------------------
 #baixando os dados de 2024 por url e instalando pacotes porque eu resetei 
 #pc recentemente.
 
@@ -12,10 +13,6 @@
 #install.packages("Rtools")
 #install.packages("installr")
 #installr::install.Rtools()
-
-#library(remotes)
-#remotes::install_github("danicat/read.dbc")
-
-#url_dbc <- "ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/DADOS/FINAIS/DENGBR24.dbc"
-#caminho_fixo <- "C:/Users/alves/Desktop/dados dengue/dados dengue 2015-2025/dados dbc/DENGBR24.dbc"
-#download.file(url_dbc, caminho_fixo, mode = "wb")
+#library(data.table)
+#library(readr)
+#library(dplyr)
